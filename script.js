5371ace9d4c8f05fcae72b91ffcb9170ce397e8c
Patricia Acorda <acordp@rpi.edu>
2019-08-12 18:56:27 -0400

    js?

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-$(document).ready(function() {
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
   
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
   
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-});
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
